3485cfd9ff6169c300be7d5c5ea41163157584ed
Park Chan Wan <pchanwan@naver.com>
2023-09-14 01:20:52 +0900

    2023.09.14 commit

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 자바 코드로 빈 등록하든, 컴포넌트 스캔을 이용하든, Controller는 사용해야한다.
 @Controller
@@ -34,4 +40,33 @@ public class MemberController {
         this.memberService = memberService;
     }
      */
+
+    @GetMapping("/members/new")
+    public String createForm () {
+        return "members/createMemberForm";
+    }
+
+    // GetMapping 은 Url 들어올때 사용하고
+    // PostMapping 은 form 에서 post 로 설정하는 것으로 들어올때 사용
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+        // 여기서 MemberForm form 은 MemberForm 의 맴버 변수인 name과
+        // html 상의 form 의 name 값과 매칭이 되어서 들어온다.
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
 }
